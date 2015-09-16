94ace08ecfaa128407227cc3de746374d8663488
LiangLUO <lluo1@stevens.edu>
2015-09-15 20:02:26 -0400

    Init Commit (Solve all Dependencies)

---
+++ OpenCV_Starter/img_wrap.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+	const char* filePath = "basketball-court.ppm";
+
+	Mat image;
+	image = imread(filePath, IMREAD_COLOR); // Read the file
+
+	if (!image.data) // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	cout << "col size:" << image.cols << endl;
+	cout << "row size:" << image.rows << endl;
+
+	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Display window", image); // Show our image inside it.
+
+	waitKey(0); // Wait for a keystroke in the window
+
+	return 0;
+}
