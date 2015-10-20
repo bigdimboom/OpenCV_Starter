5ec7ab6a2dabc981683ae907a24b8d739141652f
LiangLUO <lluo1@stevens.edu>
2015-10-19 21:18:04 -0400

    apply harris operator to left image

--- OpenCV_Starter/harris_conner_detection_and_matching.cpp
+++ OpenCV_Starter/harris_conner_detection_and_matching.cpp
@@ -4,9 +4,13 @@
 #include <opencv2/core/core.hpp>           // cv::Mat
 #include <opencv2/highgui/highgui.hpp>     // cv::imread()
 #include <string>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
+
+#define TYPE_SIZE CV_64F
+typedef double ImageType;
 
 bool loadImage(Mat& img, string path)
 {
@@ -27,7 +31,7 @@ void showImage(Mat& img, string path)
 
 void ComputeI(Mat& in, Mat& out, Mat& filter)
 {
-	out = Mat::zeros(in.rows, in.cols, CV_8U);
+	out = Mat::zeros(in.rows, in.cols, TYPE_SIZE);
 
 	for (int r = 1; r < in.rows - 1; ++r)
 	{
@@ -38,10 +42,12 @@ void ComputeI(Mat& in, Mat& out, Mat& filter)
 			{
 				for (int cc = -1; cc <= 1; ++cc)
 				{
-					sum = sum + filter.at<uchar>(rr + 1, cc + 1) * in.at<uchar>(r - rr, c - cc);
+					sum = sum + filter.at<int>(rr + 1, cc + 1) * in.at<uchar>(r - rr, c - cc);
 				}
 			}
-			out.at<uchar>(r, c) = sum;
+			sum = sum < 0 ? 0 : sum;
+			sum = sum > 255 ? 255 : sum;
+			out.at<ImageType>(r, c) = sum;
 		}
 	}
 }
@@ -49,7 +55,7 @@ void ComputeI(Mat& in, Mat& out, Mat& filter)
 void applyGaussian(Mat& in, Mat& out, double sigma = 1.0)
 {
 	// TODO: GenerateFilter
-	out = Mat::zeros(in.rows, in.cols, CV_8U);
+	out = Mat::zeros(in.rows, in.cols, TYPE_SIZE);
 
 	Mat G = Mat::zeros(5, 5, CV_64F);
 	double left = 2.0 * CV_PI * sigma * sigma;
@@ -90,12 +96,43 @@ void applyGaussian(Mat& in, Mat& out, double sigma = 1.0)
 			{
 				for (int cc = -2; cc <= 2; ++cc)
 				{
-					sum = sum + G.at<double>(rr + 2, cc + 2) * in.at<uchar>(r - rr, c - cc);
+					sum = sum + G.at<double>(rr + 2, cc + 2) * in.at<ImageType>(r - rr, c - cc);
 				}
 			}
-			out.at<uchar>(r, c) = sum;
+			out.at<ImageType>(r, c) = sum;
 		}
 	}
+}
+
+void harrisOperation(Mat& Ixx, Mat& Iyy, Mat& Ixy, Mat& out, double threshold = 2000)
+{
+	out = Mat::zeros(Ixx.rows, Ixx.cols, TYPE_SIZE);
+	// int count = 0;
+
+	for (int r = 2; r < out.rows; ++r)
+	{
+		for (int c = 2; c < out.cols; ++c)
+		{
+			double f = 0.0;
+			double xx = Ixx.at<ImageType>(r, c);
+			double yy = Iyy.at<ImageType>(r, c);
+			double xy = Ixy.at<ImageType>(r, c);
+
+			double detOfMat = xx * yy - xy * xy;
+			double traceOfMat = xx + yy;
+
+			f = detOfMat / traceOfMat; 
+			  // slides: set 4, page 61 
+
+			if (f > threshold)
+			{
+				out.at<ImageType>(r,c) = f;
+				// ++count;
+			}
+		}
+	}
+
+	// cout << count;
 }
 
 int main(int argc, char** argv)
@@ -119,15 +156,26 @@ int main(int argc, char** argv)
 	}
 
 	// STEP 1: compute Is
-	Mat leftI_x, leftI_y, leftI_x_smooth, leftI_y_smooth;
-	Mat W_x = (Mat_<uchar>(3, 3) << -1, 0, 1, -1, 0, 1, -1, 0, 1);
-	Mat W_y = (Mat_<uchar>(3, 3) << -1, -1, -1, 0, 0, 0, 1, 1, 1);
-	ComputeI(leftImg, leftI_x, W_x);
-	ComputeI(leftImg, leftI_y, W_y);
+	Mat left_Ix, left_Iy, left_Ixx, left_Iyy, left_Ixy;
+	Mat W_x = (Mat_<int>(3, 3) << -1, 0, 1, -1, 0, 1, -1, 0, 1);
+	Mat W_y = (Mat_<int>(3, 3) << -1, -1, -1, 0, 0, 0, 1, 1, 1);
+
+	ComputeI(leftImg, left_Ix, W_x);
+	ComputeI(leftImg, left_Iy, W_y);
+
+	left_Ixx = left_Ix.mul(left_Ix); // I(x*x)
+	left_Iyy = left_Iy.mul(left_Iy); // I(y*y)
+	left_Ixy = left_Ix.mul(left_Iy); // I(x*y)
 
 	// STEP 2: Apply Gussian Smooth Filter
-	applyGaussian(leftI_x, leftI_x_smooth);
-	applyGaussian(leftI_y, leftI_y_smooth);
+	Mat left_Ixx_smooth, left_Iyy_smooth, left_Ixy_smooth;
+	applyGaussian(left_Ixx, left_Ixx_smooth);
+	applyGaussian(left_Iyy, left_Iyy_smooth);
+	applyGaussian(left_Ixy, left_Ixy_smooth);
+
+	// STEP 3: Apply Harris Operator
+	Mat leftOut;
+	harrisOperation(left_Ixx_smooth, left_Iyy_smooth, left_Ixy_smooth, leftOut);
 
 	//showImage(leftImg, leftPath);
 	//showImage(rightImg, rightPath);
